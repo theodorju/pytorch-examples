504cf816fa3911221c390c7df26297bca46c230b
theodorju <theodorathanasiadis@gmail.com>
2024-11-28 10:02:59 +0100

    decouple neps_utils and main.py

--- vae/neps_utils.py
+++ vae/neps_utils.py
@@ -13,9 +13,20 @@ from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from neps.utils.common import load_checkpoint, save_checkpoint
 from neps_global_utils import set_seed, process_trajectory
-from main import loss_function
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE + KLD
 
 class VAE(nn.Module):
     def __init__(self):
