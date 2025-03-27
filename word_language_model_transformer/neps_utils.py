2477c2d1c59d81757ab29d265da6d267cda600c8
theodorju <theodorathanasiadis@gmail.com>
2025-03-27 13:52:24 +0100

    add a variant of word_language_model with transformer as the underlying model

---
+++ word_language_model_transformer/neps_utils.py
@@ -0,0 +1,233 @@
+=Noneimport sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import time
+import torch
+import numpy as np
+import neps
+from model import TransformerModel, RNNModel
+from neps.utils.common import load_checkpoint, save_checkpoint
+from neps_global_utils import process_trajectory
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_batch(source, i, bptt):
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i : i + seq_len]
+    target = source[i + 1 : i + 1 + seq_len].view(-1)
+    return data, target
+
+
+def batchify(data, bsz):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    return data.to(device)
+
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Tensors, to detach them from their history."""
+
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def evaluate(model, criterion, data, ntokens, eval_batch_size, bptt):
+    model.eval()
+    val_loss = 0.0
+    if not isinstance(model, TransformerModel):
+        hidden = model.init_hidden(eval_batch_size)
+    with torch.no_grad():
+        for i in range(0, data.size(0) - 1, bptt):
+            input, targets = get_batch(data, i, bptt)
+            if isinstance(model, TransformerModel):
+                output = model(input)
+                output = output.view(-1, ntokens)
+            else:
+                output, hidden = model(input, hidden)
+                hidden = repackage_hidden(hidden)
+            val_loss += len(input) * criterion(output, targets).item()
+    return val_loss / (len(data) - 1)
+
+
+def train_epoch(
+    model,
+    optimizer,
+    criterion,
+    ntokens,
+    train_data,
+    val_data,
+    eval_batch_size,
+    batch_size,
+    bptt,
+    clip,
+    n_params=4,
+    l1=None, l2=None,
+):
+    model.train()
+
+    if not isinstance(model, TransformerModel):
+        hidden = model.init_hidden(batch_size)
+
+    for _, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i, bptt)
+        optimizer.zero_grad()
+        if isinstance(model, TransformerModel):
+            output = model(data)
+            output = output.view(-1, ntokens)
+        else:
+            hidden = repackage_hidden(hidden)
+            output, hidden = model(data, hidden)
+        loss = criterion(output, targets)
+        if n_params == 8:
+            l1_loss = 0
+            l2_loss = 0
+            # apply l1 and l2 regularization
+            for p in model.parameters():
+                l1_loss += torch.sum(torch.abs(p))
+                l2_loss += torch.sum(p ** 2)
+            loss += l1 * l1_loss + l2 * l2_loss
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+
+    val_loss = evaluate(model, criterion, val_data, ntokens, eval_batch_size, bptt)
+
+    return val_loss
+
+
+def run_pipeline(
+    pipeline_directory,
+    previous_pipeline_directory,
+    learning_rate,
+    beta1=None,
+    beta2=None,
+    epsilon=None,
+    epoch=50,  # 50 default if not handled by the searcher
+    l1=None,
+    l2=None,
+    linear_decay=None,
+    exponential_decay=None,
+    opts=None,
+    corpus=None,
+    eval_batch_size=10,
+    n_params=4
+):
+    start = time.time()
+    epochs = int(epoch)
+
+    criterion = torch.nn.NLLLoss()
+
+    ntokens = len(corpus.dictionary)
+    if opts.model == "Transformer":
+        model = TransformerModel(
+            ntokens, opts.emsize, opts.nhead, opts.nhid, opts.nlayers, opts.dropout
+        ).to(device)
+    else:
+        model = RNNModel(
+            opts.model,
+            ntokens,
+            opts.emsize,
+            opts.nhid,
+            opts.nlayers,
+            opts.dropout,
+            opts.tied,
+        ).to(device)
+
+    if n_params == 1:
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    
+    else:
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=learning_rate, betas=(beta1, beta2), eps=epsilon
+        )
+
+    previous_state = load_checkpoint(
+        directory=previous_pipeline_directory, model=model, optimizer=optimizer
+    )
+
+    start_epoch = previous_state["epochs_trained"] if previous_state is not None else 0
+
+    train_data = batchify(corpus.train, opts.batch_size)
+    val_data = batchify(corpus.valid, eval_batch_size)
+    test_data = batchify(corpus.test, eval_batch_size)
+
+    val_losses, test_losses = [], []
+
+    for ep in range(start_epoch, epochs):
+        print("  Epoch {} / {} ...".format(ep + 1, epochs).ljust(2))
+        if n_params == 8:
+            linear_factor = np.max(1 - linear_decay * ep, 0)
+            exponential_factor = np.exp(-exponential_decay * ep)
+            updated_lr = learning_rate * linear_factor * exponential_factor
+            # update lr manually
+            assert len(optimizer.param_groups) == 1
+            optimizer.param_groups[0]["lr"] = updated_lr
+        
+        val_loss = train_epoch(
+            model,
+            optimizer,
+            criterion,
+            ntokens,
+            train_data,
+            val_data,
+            eval_batch_size,
+            opts.batch_size,
+            opts.bptt,
+            opts.clip,
+            n_params,
+            l1, l2,
+        )
+        val_losses.append(val_loss)
+        test_loss = evaluate(
+            model, criterion, test_data, ntokens, eval_batch_size, opts.bptt
+        )
+        test_losses.append(test_loss)
+
+    save_checkpoint(
+        directory=pipeline_directory,
+        model=model,
+        optimizer=optimizer,
+        values_to_save={
+            "epochs_trained": epochs,
+        },
+    )
+
+    end = time.time()
+
+    print(f"  Epoch {epochs} / {epochs} Val Loss: {val_loss}".ljust(2))
+    
+    learning_curves, min_valid_seen, min_test_seen = process_trajectory(
+        pipeline_directory, val_loss, val_losses, test_losses, test_loss
+    )
+
+    return {
+        "cost": epochs - start_epoch,
+        "info_dict": {
+            "continuation_fidelity": None,
+            "cost": epochs - start_epoch,
+            "end_time": end,
+            "fidelity": epochs,
+            "learning_curve": val_losses,
+            "learning_curves": learning_curves,
+            "max_fidelity_cost": epochs,
+            "max_fidelity_loss": val_losses[-1],
+            # "min_test_ever": np.min(test_losses),
+            "min_test_seen": np.min(learning_curves["test"]),
+            # "min_valid_ever": np.min(val_losses),
+            "min_valid_seen": np.min(learning_curves["valid"]),
+            "process_id": os.getpid(),
+            "start_time": start,
+            "test_score": test_loss,
+            "val_score": -val_loss,
+        },
+        "loss": val_loss,
+    }
---
+++ word_language_model_transformer/run_neps.py
@@ -0,0 +1,144 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import argparse
+import logging
+import neps
+import torch
+import data
+from neps_utils import run_pipeline
+from neps_global_utils import set_seed, create_3d_plot, get_pipeline_space, get_neps_root_directory
+from functools import partial
+
+def main(args):
+    set_seed(args.seed)
+    pipeline_space = get_pipeline_space(args.searcher, n_params=args.n_params)
+    logging.basicConfig(level=logging.INFO)
+    corpus = data.Corpus(args.data)
+
+    run_pipeline_partial = partial(run_pipeline, opts=args, corpus=corpus, eval_batch_size=args.eval_batch_size, n_params=args.n_params)
+
+    neps_root_directory = get_neps_root_directory(args.n_params, "word_lm", args.searcher, args.seed)
+
+    ifbo_alternative = "ifbo" in args.searcher or "mixup" in args.searcher or "cdf" in args.searcher
+
+    if not args.plot_only:
+        neps_kwargs = {
+            "run_pipeline": run_pipeline_partial,
+            "pipeline_space": pipeline_space,
+            "root_directory": neps_root_directory,
+            "overwrite_working_directory": args.overwrite_working_directory,
+            "max_cost_total": args.max_cost_total,
+            "searcher": args.searcher,
+            "searcher_path": args.searcher_path,
+            "post_run_summary": True,
+        }
+        if ifbo_alternative:
+            neps_kwargs["surrogate_model_args"] = {
+                # "soft_ub": 10.412651796975453,  # np.log(len(corpus.dictionary)=33278)
+                # "soft_lb": 0.0,
+                # "lb": 0.0,
+                # "normalization_method": "pfn",
+                "normalization_method": "neps",
+                "max_value": 10.5, # empirical value from epoch 0 after a few runs
+            }
+        neps.run(**neps_kwargs)
+
+    if ifbo_alternative: # includes any ifbo variant
+        create_3d_plot(
+            args.searcher,
+            args.seed,
+            neps_root_directory,
+            benchmark="word_lm",
+            normalization_method="neps",
+            max_value=10.5,
+            soft_lb=torch.tensor(0.0),
+            soft_ub=torch.tensor(10.412651796975453),
+            lb=torch.tensor(0.0),
+            minimize=False,
+        )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="word language model ifbo")
+    parser.add_argument("--searcher", type=str, default="ifbo")
+    parser.add_argument("--max_cost_total", type=int, default=50)
+    parser.add_argument("--overwrite_working_directory", action="store_true")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument(
+        "--searcher_path",
+        type=str,
+        default="/home/theo/development/automl/ta_forks/tj-ifbo_private/src/pfns_hpo/pfns_hpo/configs/algorithm",
+    )
+
+    # example specific arguments
+    parser.add_argument(
+        "--data",
+        type=str,
+        default="./data/wikitext-2",
+        help="location of the data corpus",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="Transformer",
+        help="type of network (RNN_TANH, RNN_RELU, LSTM, GRU, Transformer)",
+    )
+    parser.add_argument(
+        "--emsize", type=int, default=200, help="size of word embeddings"
+    )
+    parser.add_argument(
+        "--nhid", type=int, default=200, help="number of hidden units per layer"
+    )
+    parser.add_argument("--nlayers", type=int, default=2, help="number of layers")
+    parser.add_argument("--clip", type=float, default=0.25, help="gradient clipping")
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=20,
+    )
+    parser.add_argument(
+        "--eval_batch_size",
+        type=int,
+        default=10,
+    )
+    parser.add_argument("--bptt", type=int, default=35, help="sequence length")
+    parser.add_argument(
+        "--dropout",
+        type=float,
+        default=0.2,
+        help="dropout applied to layers (0 = no dropout)",
+    )
+    parser.add_argument(
+        "--tied", action="store_true", help="tie the word embedding and softmax weights"
+    )
+    # parser.add_argument("--cuda", action="store_true", default=False, help="use CUDA")
+    # parser.add_argument(
+    #     "--mps", action="store_true", default=False, help="enables macOS GPU training"
+    # )
+    # parser.add_argument(
+    #     "--log-interval", type=int, default=200, metavar="N", help="report interval"
+    # )
+    # parser.add_argument(
+    #     "--save", type=str, default="model.pt", help="path to save the final model"
+    # )
+    # parser.add_argument(
+    #     "--onnx-export",
+    #     type=str,
+    #     default="",
+    #     help="path to export the final model in onnx format",
+    # )
+    parser.add_argument(
+        "--nhead",
+        type=int,
+        default=2,
+        help="the number of heads in the encoder/decoder of the transformer model",
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", help="verify the code and the model"
+    )
+    parser.add_argument("--plot_only", action="store_true")
+    parser.add_argument("--n_params", type=int, default=4)
+    args = parser.parse_args()
+    main(args)
